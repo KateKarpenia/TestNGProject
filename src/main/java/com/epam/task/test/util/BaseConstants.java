07115b43253caaa1d344a5a4e37e9d5c2068c230
Katerina Karpenia <Katerina.Karpenia@thomsonreuters.com>
2020-04-09 14:41:47 +0300

    added TestNGConfigurator

--- src/main/java/com/epam/task/test/util/BaseConstants.java
+++ src/main/java/com/epam/task/test/util/BaseConstants.java
@@ -2,11 +2,12 @@ package com.epam.task.test.util;
 
 public class BaseConstants
 {
-	public static final String SUIT_NAME = "Suite 1";
+	public static final String SUIT_NAME = "Suite_";
 	public static final String FILE_PATH_PROP = "filePath";
 	public static final String DELIMITER_PROP = "delimiter";
 	public static final String CLASS_PATH = "com.epam.task.test.tests.";
-	public static final String TEST_NAME = "Test 1";
+	public static final String TEST_NAME = "Test_";
 	public static final String PARAM_1 = "param1";
 	public static final String PARAM_2 = "param2";
+	public static final String CONFIG = "config";
 }
